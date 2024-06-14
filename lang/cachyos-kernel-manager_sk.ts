b81a9064c1ac7ade350c41f8017583056399fd02
vawaver <vawaver@vawav.com>
2024-06-14 10:22:14 +0200

    slovak translation

---
+++ lang/cachyos-kernel-manager_sk.ts
@@ -0,0 +1,280 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk">
+<context>
+    <name>ConfOptionsPage</name>
+    <message>
+        <source>Enable CachyOS config</source>
+        <translation>Povoliť konfiguráciu CachyOS</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via nconfig</source>
+        <translation>Upraviť možnosti jadra pred zostavením cez nconfig</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via menuconfig</source>
+        <translation>Upraviť možnosti jadra pred zostavením cez menuconfig</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via xconfig</source>
+        <translation>Upraviť možnosti jadra pred zostavením cez xconfig</translation>
+    </message>
+    <message>
+        <source>Tweak kernel options prior to a build via gconfig</source>
+        <translation>Upraviť možnosti jadra pred zostavením cez gconfig</translation>
+    </message>
+    <message>
+        <source>Disable NUMA</source>
+        <translation>Zakázať NUMA</translation>
+    </message>
+    <message>
+        <source>Use Modprobed-db</source>
+        <translation>Použiť Modprobed-db</translation>
+    </message>
+    <message>
+        <source>Enable KBUILD_CFLAGS -O3</source>
+        <translation>Povoliť KBUILD_CFLAGS -O3</translation>
+    </message>
+    <message>
+        <source>Set performance governor as default</source>
+        <translation>Nastaviť správcu výkonu ako predvoleného</translation>
+    </message>
+    <message>
+        <source>Enable TCP_CONG_BBR3</source>
+        <translation>Povoliť TCP_CONG_BBR3</translation>
+    </message>
+    <message>
+        <source>Running tick rate</source>
+        <translation>Vybrať "tick rate"</translation>
+    </message>
+    <message>
+        <source>Select tickless</source>
+        <translation>Vybrať "tickless"</translation>
+    </message>
+    <message>
+        <source>Select preempt</source>
+        <translation>Vybrať "preempt"</translation>
+    </message>
+    <message>
+        <source>Enable multigenerational LRU</source>
+        <translation>Povoliť multigeneračný LRU</translation>
+    </message>
+    <message>
+        <source>Enable per-VMA locking</source>
+        <translation>Povoliť zamykanie per-VMA</translation>
+    </message>
+    <message>
+        <source>Enable DAMON</source>
+        <translation>Povoliť DAMON</translation>
+    </message>
+    <message>
+        <source>CPU compiler optimizations</source>
+        <translation>Vybrať optimalizácie CPU</translation>
+    </message>
+    <message>
+        <source>Apply automatic CPU Optimization</source>
+        <translation>Použiť automatické optimalizácie CPU</translation>
+    </message>
+    <message>
+        <source>Enable LTO</source>
+        <translation>Povoliť LTO</translation>
+    </message>
+    <message>
+        <source>Build the ZFS module</source>
+        <translation>Vytvoriť modul ZFS</translation>
+    </message>
+    <message>
+        <source>Transparent Hugepages</source>
+        <translation>Nastaviť Transparent Hugepages</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Zrušiť</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Spustiť</translation>
+    </message>
+    <message>
+        <source>Custom package name</source>
+        <translation>Vlastný názov balíka</translation>
+    </message>
+    <message>
+        <source>$pkgbase</source>
+        <translation>$pkgbase</translation>
+    </message>
+    <message>
+        <source>Build the NVIDIA module</source>
+        <translation>Vytvoriť modul NVIDIA</translation>
+    </message>
+</context>
+<context>
+    <name>ConfPatchesPage</name>
+    <message>
+        <source>Add remote patch</source>
+        <translation>Pridať vzdialený patch</translation>
+    </message>
+    <message>
+        <source>Add local patch</source>
+        <translation>Pridať lokálny patch</translation>
+    </message>
+</context>
+<context>
+    <name>ConfWindow</name>
+    <message>
+        <source>CachyOS Kernel Manager Configure</source>
+        <translation>Konfigurácia správcu jadier CachyOS</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Možnosti</translation>
+    </message>
+    <message>
+        <source>Patches</source>
+        <translation>Patche</translation>
+    </message>
+    <message>
+        <source>Bore - Burst-Oriented Response Enhancer</source>
+        <translation>Plánovač BORE - Burst-Oriented Response Enhancer</translation>
+    </message>
+    <message>
+        <source>RC - Release Candidate</source>
+        <translation>RC - Release Candidate</translation>
+    </message>
+    <message>
+        <source>Select one or more patch files</source>
+        <translation>Vybrať jeden alebo viac patch súborov</translation>
+    </message>
+    <message>
+        <source>Patch file (*.patch)</source>
+        <translation>Súbor patch (*.patch)</translation>
+    </message>
+    <message>
+        <source>Enter URL patch</source>
+        <translation>Zadajte URL patch</translation>
+    </message>
+    <message>
+        <source>Patch URL:</source>
+        <translation>Patch URL:</translation>
+    </message>
+    <message>
+        <source>RT - Realtime kernel</source>
+        <translation>RT - Realtime jadro</translation>
+    </message>
+    <message>
+        <source>RT-Bore</source>
+        <translation>RT-Bore</translation>
+    </message>
+    <message>
+        <source>Sched-Ext - BPF extensible scheduler class</source>
+        <translation>Sched-Ext - BPF rozšíriteľná trieda plánovača</translation>
+    </message>
+    <message>
+        <source>CachyOS - BORE + SCHED-EXT</source>
+        <translation>CachyOS - BORE + SCHED-EXT</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>CachyOS Kernel Manager</source>
+        <translation>Správca jadier CachyOS</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
+&lt;p&gt;You can install and/or uninstall kernel packages using the checkboxes on the leftmost column.&lt;/p&gt;
+&lt;p&gt;This app won&apos;t work, if you are already running a pacman instance.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</source>
+        <translation>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Tu nájdete informácie o aktuálne nainštalovaných a dostupných Linuxových jadrách.&lt;/p&gt;
+&lt;p&gt;Môžete inštalovať a/alebo odinštalovať balíky jadra pomocou zaškrtávacích políčok v ľavom stĺpci.&lt;/p&gt;
+&lt;p&gt;Táto aplikácia nebude fungovať, ak už beží inštancia pacmana.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Choose</source>
+        <translation>Vybrať</translation>
+    </message>
+    <message>
+        <source>PkgName</source>
+        <translation>Názov balíčka</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Verzia</translation>
+    </message>
+    <message>
+        <source>Category</source>
+        <translation>Kategória</translation>
+    </message>
+    <message>
+        <source>Configure</source>
+        <translation>Konfigurovať</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Zrušiť</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Spustiť</translation>
+    </message>
+    <message>
+        <source>No kernels found!
+Please run `pacman -Sy` to update DB!
+This is needed for the app to work properly</source>
+        <translation>Žiadne jadrá neboli nájdené!
+Prosím, spustite `pacman -Sy` na aktualizáciu DB!
+To je potrebné na správne fungovanie aplikácie</translation>
+    </message>
+    <message>
+        <source>Failed to clone repository!
+Please check your internet connection and try again</source>
+        <translation>Nepodarilo sa klonovať repozitár!
+Skontrolujte svoje internetové pripojenie a skúste to znova</translation>
+    </message>
+    <message>
+        <source>Please wait...
+We are preparing configuration window for you
+cloning PKGBUILDs..</source>
+        <translation>Prosím, čakajte...
+Pripravujeme konfiguračné okno pre vás
+klonovanie PKGBUILDs..</translation>
+    </message>
+    <message>
+        <source>Failed to initialize alpm handle (%1)</source>
+        <translation>Nepodarilo sa inicializovať alpm handle (%1)</translation>
+    </message>
+    <message>
+        <source>Failed to release alpm handle (%1)</source>
+        <translation>Nepodarilo sa uvoľniť alpm handle (%1)</translation>
+    </message>
+    <message>
+        <source>Please wait...
+Initializing kernels..</source>
+        <translation>Prosím, čakajte...
+Inicializácia jadier..</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
+&lt;p&gt;You can install/uninstall kernel packages using the checkboxes on the leftmost column.&lt;/p&gt;
+&lt;p&gt;This app won&apos;t work if you are already running a pacman instance.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</source>
+        <translation>&lt;html&gt;
+&lt;body&gt;
+&lt;p&gt;Tu nájdete informácie o aktuálne nainštalovaných a dostupných Linuxových jadrách.&lt;/p&gt;
+&lt;p&gt;Môžete inštalovať a/alebo odinštalovať balíky jadra pomocou zaškrtávacích políčok v ľavom stĺpci.&lt;/p&gt;
+&lt;p&gt;Táto aplikácia nebude fungovať, ak už beží inštancia pacmana.&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;</translation>
+    </message>
+</context>
+</TS>
