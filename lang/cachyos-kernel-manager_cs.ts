d1662b24f8e365acd1b44a13fdca9d0d0e20d7e7
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2024-06-24 21:45:13 +0400

    ♻  update sk and cs translations

--- lang/cachyos-kernel-manager_cs.ts
+++ lang/cachyos-kernel-manager_cs.ts
@@ -4,106 +4,139 @@
 <context>
     <name>ConfOptionsPage</name>
     <message>
+        <location filename="../src/conf-options-page.ui" line="66"/>
         <source>Enable CachyOS config</source>
         <translation>Povolit konfiguraci CachyOS</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="95"/>
         <source>Tweak kernel options prior to a build via nconfig</source>
         <translation>Upravit možnosti jádra před sestavením přes nconfig</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="124"/>
         <source>Tweak kernel options prior to a build via menuconfig</source>
         <translation>Upravit možnosti jádra před sestavením přes menuconfig</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="153"/>
         <source>Tweak kernel options prior to a build via xconfig</source>
         <translation>Upravit možnosti jádra před sestavením přes xconfig</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="182"/>
         <source>Tweak kernel options prior to a build via gconfig</source>
         <translation>Upravit možnosti jádra před sestavením přes gconfig</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="211"/>
         <source>Disable NUMA</source>
         <translation>Zakázat NUMA</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="240"/>
         <source>Use Modprobed-db</source>
         <translation>Použít Modprobed-db</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="269"/>
         <source>Enable KBUILD_CFLAGS -O3</source>
         <translation>Povolit KBUILD_CFLAGS -O3</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="298"/>
         <source>Set performance governor as default</source>
         <translation>Nastavit správce výkonu jako preferovaného</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="327"/>
         <source>Enable TCP_CONG_BBR3</source>
         <translation>Povolit TCP_CONG_BBR3</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="356"/>
         <source>Running tick rate</source>
-        <translation>Vybrat "tick rate"</translation>
+        <translation>Vybrat &quot;tick rate&quot;</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="385"/>
         <source>Select tickless</source>
-        <translation>Vybrat "tickless"</translation>
+        <translation>Vybrat &quot;tickless&quot;</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="414"/>
         <source>Select preempt</source>
-        <translation>Vybrat "preempt"</translation>
+        <translation>Vybrat &quot;preempt&quot;</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="712"/>
+        <source>Build kernel</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Enable multigenerational LRU</source>
-        <translation>Povolit multigenerační LRU</translation>
+        <translation type="vanished">Povolit multigenerační LRU</translation>
     </message>
     <message>
         <source>Enable per-VMA locking</source>
-        <translation>Povolit zamykání per-VMA</translation>
+        <translation type="vanished">Povolit zamykání per-VMA</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="443"/>
         <source>Enable DAMON</source>
         <translation>Povolit DAMON</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="472"/>
         <source>CPU compiler optimizations</source>
         <translation>Vybrat optimalizace CPU</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="501"/>
         <source>Apply automatic CPU Optimization</source>
         <translation>Použít automatické optimalizace CPU</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="530"/>
         <source>Enable LTO</source>
         <translation>Povolit LTO</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="559"/>
         <source>Build the ZFS module</source>
         <translation>Vytvořit modul ZFS</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="617"/>
+        <source>Include vmlinux with debug informations/symbols</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="646"/>
         <source>Transparent Hugepages</source>
         <translation>Nastavit Transparent Hugepages</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="705"/>
         <source>Cancel</source>
         <translation>Zrušit</translation>
     </message>
     <message>
         <source>Execute</source>
-        <translation>Spustit</translation>
+        <translation type="vanished">Spustit</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="33"/>
         <source>Custom package name</source>
         <translation>Vlastní název balíčku</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="53"/>
         <source>$pkgbase</source>
         <translation>$pkgbase</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="588"/>
         <source>Build the NVIDIA module</source>
         <translation>Vytvořit modul NVIDIA</translation>
     </message>
@@ -111,10 +144,12 @@
 <context>
     <name>ConfPatchesPage</name>
     <message>
+        <location filename="../src/conf-patches-page.ui" line="81"/>
         <source>Add remote patch</source>
         <translation>Přidat vzdálený patch</translation>
     </message>
     <message>
+        <location filename="../src/conf-patches-page.ui" line="88"/>
         <source>Add local patch</source>
         <translation>Přidat lokální patch</translation>
     </message>
@@ -122,54 +157,67 @@
 <context>
     <name>ConfWindow</name>
     <message>
+        <location filename="../src/conf-window.ui" line="17"/>
         <source>CachyOS Kernel Manager Configure</source>
         <translation>Konfigurace správce jader CachyOS</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.ui" line="40"/>
         <source>Options</source>
         <translation>Možnosti</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.ui" line="45"/>
         <source>Patches</source>
         <translation>Patche</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="378"/>
         <source>Bore - Burst-Oriented Response Enhancer</source>
         <translation>Plánovač BORE - Burst-Oriented Response Enhancer</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="379"/>
         <source>RC - Release Candidate</source>
         <translation>RC - Release Candidate</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="455"/>
         <source>Select one or more patch files</source>
         <translation>Vybrat jeden nebo víc patch souborů</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="457"/>
         <source>Patch file (*.patch)</source>
         <translation>Soubor patch (*.patch)</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="477"/>
         <source>Enter URL patch</source>
         <translation>Zadejte URL patch</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="478"/>
         <source>Patch URL:</source>
         <translation>Patch URL:</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="380"/>
         <source>RT - Realtime kernel</source>
         <translation>RT - Realtime jadro</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="381"/>
         <source>RT-Bore</source>
         <translation>RT-Bore</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="382"/>
         <source>Sched-Ext - BPF extensible scheduler class</source>
         <translation>Sched-Ext - BPF rozšířitelná třída plánovače</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="377"/>
         <source>CachyOS - BORE + SCHED-EXT</source>
         <translation>CachyOS - BORE + SCHED-EXT</translation>
     </message>
@@ -177,6 +225,7 @@
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../src/km-window.ui" line="17"/>
         <source>CachyOS Kernel Manager</source>
         <translation>Správce jader CachyOS</translation>
     </message>
@@ -188,7 +237,7 @@
 &lt;p&gt;This app won&apos;t work, if you are already running a pacman instance.&lt;/p&gt;
 &lt;/body&gt;
 &lt;/html&gt;</source>
-        <translation>&lt;html&gt;
+        <translation type="vanished">&lt;html&gt;
 &lt;body&gt;
 &lt;p&gt;zde nejdete informace o aktuálně nainstalovaných a dostupných Linuxových jádrách.&lt;/p&gt;
 &lt;p&gt;Můžete instalovat a/nebo odinstalovat balíčky jádra pomocí zaškrtávacích políček v levém sloupci.&lt;/p&gt;
@@ -197,34 +246,47 @@
 &lt;/html&gt;</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="64"/>
         <source>Choose</source>
         <translation>Vybrat</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="69"/>
         <source>PkgName</source>
         <translation>Název balíčku</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="74"/>
         <source>Version</source>
         <translation>Verze</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="79"/>
         <source>Category</source>
         <translation>Kategorie</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="116"/>
+        <source>sched-ext scheduler config</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="123"/>
         <source>Configure</source>
         <translation>Konfigurovat</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="130"/>
         <source>Cancel</source>
         <translation>Zrušit</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="137"/>
         <source>Execute</source>
         <translation>Spustit</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="228"/>
         <source>No kernels found!
 Please run `pacman -Sy` to update DB!
 This is needed for the app to work properly</source>
@@ -233,12 +295,15 @@ Prosím, spusťte `pacman -Sy` pro aktualizaci DB!
 To je potřebné na správné fungování aplikace</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="202"/>
         <source>Failed to clone repository!
 Please check your internet connection and try again</source>
         <translation>Nepodařilo se klonovat repozitář!
 Zkontrolujte svoje internetové připojení a zkuste to znovu</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="277"/>
+        <location filename="../src/km-window.cpp" line="341"/>
         <source>Please wait...
 We are preparing configuration window for you
 cloning PKGBUILDs..</source>
@@ -247,20 +312,24 @@ Připravujeme konfigurační okno pro vás
 klonování PKGBUILDs..</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="144"/>
         <source>Failed to initialize alpm handle (%1)</source>
         <translation>Nepodařilo se inicializovat alpm handle (%1)</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="157"/>
         <source>Failed to release alpm handle (%1)</source>
         <translation>Nepodařilo se uvolniť alpm handle (%1)</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="362"/>
         <source>Please wait...
 Initializing kernels..</source>
         <translation>Prosím, čekejte...
 Inicializáce jader..</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="28"/>
         <source>&lt;html&gt;
 &lt;body&gt;
 &lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
--- lang/cachyos-kernel-manager_sk.ts
+++ lang/cachyos-kernel-manager_sk.ts
@@ -4,106 +4,139 @@
 <context>
     <name>ConfOptionsPage</name>
     <message>
+        <location filename="../src/conf-options-page.ui" line="66"/>
         <source>Enable CachyOS config</source>
         <translation>Povoliť konfiguráciu CachyOS</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="95"/>
         <source>Tweak kernel options prior to a build via nconfig</source>
         <translation>Upraviť možnosti jadra pred zostavením cez nconfig</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="124"/>
         <source>Tweak kernel options prior to a build via menuconfig</source>
         <translation>Upraviť možnosti jadra pred zostavením cez menuconfig</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="153"/>
         <source>Tweak kernel options prior to a build via xconfig</source>
         <translation>Upraviť možnosti jadra pred zostavením cez xconfig</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="182"/>
         <source>Tweak kernel options prior to a build via gconfig</source>
         <translation>Upraviť možnosti jadra pred zostavením cez gconfig</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="211"/>
         <source>Disable NUMA</source>
         <translation>Zakázať NUMA</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="240"/>
         <source>Use Modprobed-db</source>
         <translation>Použiť Modprobed-db</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="269"/>
         <source>Enable KBUILD_CFLAGS -O3</source>
         <translation>Povoliť KBUILD_CFLAGS -O3</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="298"/>
         <source>Set performance governor as default</source>
         <translation>Nastaviť správcu výkonu ako predvoleného</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="327"/>
         <source>Enable TCP_CONG_BBR3</source>
         <translation>Povoliť TCP_CONG_BBR3</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="356"/>
         <source>Running tick rate</source>
-        <translation>Vybrať "tick rate"</translation>
+        <translation>Vybrať &quot;tick rate&quot;</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="385"/>
         <source>Select tickless</source>
-        <translation>Vybrať "tickless"</translation>
+        <translation>Vybrať &quot;tickless&quot;</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="414"/>
         <source>Select preempt</source>
-        <translation>Vybrať "preempt"</translation>
+        <translation>Vybrať &quot;preempt&quot;</translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="712"/>
+        <source>Build kernel</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Enable multigenerational LRU</source>
-        <translation>Povoliť multigeneračný LRU</translation>
+        <translation type="vanished">Povoliť multigeneračný LRU</translation>
     </message>
     <message>
         <source>Enable per-VMA locking</source>
-        <translation>Povoliť zamykanie per-VMA</translation>
+        <translation type="vanished">Povoliť zamykanie per-VMA</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="443"/>
         <source>Enable DAMON</source>
         <translation>Povoliť DAMON</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="472"/>
         <source>CPU compiler optimizations</source>
         <translation>Vybrať optimalizácie CPU</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="501"/>
         <source>Apply automatic CPU Optimization</source>
         <translation>Použiť automatické optimalizácie CPU</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="530"/>
         <source>Enable LTO</source>
         <translation>Povoliť LTO</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="559"/>
         <source>Build the ZFS module</source>
         <translation>Vytvoriť modul ZFS</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="617"/>
+        <source>Include vmlinux with debug informations/symbols</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/conf-options-page.ui" line="646"/>
         <source>Transparent Hugepages</source>
         <translation>Nastaviť Transparent Hugepages</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="705"/>
         <source>Cancel</source>
         <translation>Zrušiť</translation>
     </message>
     <message>
         <source>Execute</source>
-        <translation>Spustiť</translation>
+        <translation type="vanished">Spustiť</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="33"/>
         <source>Custom package name</source>
         <translation>Vlastný názov balíka</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="53"/>
         <source>$pkgbase</source>
         <translation>$pkgbase</translation>
     </message>
     <message>
+        <location filename="../src/conf-options-page.ui" line="588"/>
         <source>Build the NVIDIA module</source>
         <translation>Vytvoriť modul NVIDIA</translation>
     </message>
@@ -111,10 +144,12 @@
 <context>
     <name>ConfPatchesPage</name>
     <message>
+        <location filename="../src/conf-patches-page.ui" line="81"/>
         <source>Add remote patch</source>
         <translation>Pridať vzdialený patch</translation>
     </message>
     <message>
+        <location filename="../src/conf-patches-page.ui" line="88"/>
         <source>Add local patch</source>
         <translation>Pridať lokálny patch</translation>
     </message>
@@ -122,54 +157,67 @@
 <context>
     <name>ConfWindow</name>
     <message>
+        <location filename="../src/conf-window.ui" line="17"/>
         <source>CachyOS Kernel Manager Configure</source>
         <translation>Konfigurácia správcu jadier CachyOS</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.ui" line="40"/>
         <source>Options</source>
         <translation>Možnosti</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.ui" line="45"/>
         <source>Patches</source>
         <translation>Patche</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="378"/>
         <source>Bore - Burst-Oriented Response Enhancer</source>
         <translation>Plánovač BORE - Burst-Oriented Response Enhancer</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="379"/>
         <source>RC - Release Candidate</source>
         <translation>RC - Release Candidate</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="455"/>
         <source>Select one or more patch files</source>
         <translation>Vybrať jeden alebo viac patch súborov</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="457"/>
         <source>Patch file (*.patch)</source>
         <translation>Súbor patch (*.patch)</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="477"/>
         <source>Enter URL patch</source>
         <translation>Zadajte URL patch</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="478"/>
         <source>Patch URL:</source>
         <translation>Patch URL:</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="380"/>
         <source>RT - Realtime kernel</source>
         <translation>RT - Realtime jadro</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="381"/>
         <source>RT-Bore</source>
         <translation>RT-Bore</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="382"/>
         <source>Sched-Ext - BPF extensible scheduler class</source>
         <translation>Sched-Ext - BPF rozšíriteľná trieda plánovača</translation>
     </message>
     <message>
+        <location filename="../src/conf-window.cpp" line="377"/>
         <source>CachyOS - BORE + SCHED-EXT</source>
         <translation>CachyOS - BORE + SCHED-EXT</translation>
     </message>
@@ -177,6 +225,7 @@
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../src/km-window.ui" line="17"/>
         <source>CachyOS Kernel Manager</source>
         <translation>Správca jadier CachyOS</translation>
     </message>
@@ -188,7 +237,7 @@
 &lt;p&gt;This app won&apos;t work, if you are already running a pacman instance.&lt;/p&gt;
 &lt;/body&gt;
 &lt;/html&gt;</source>
-        <translation>&lt;html&gt;
+        <translation type="vanished">&lt;html&gt;
 &lt;body&gt;
 &lt;p&gt;Tu nájdete informácie o aktuálne nainštalovaných a dostupných Linuxových jadrách.&lt;/p&gt;
 &lt;p&gt;Môžete inštalovať a/alebo odinštalovať balíky jadra pomocou zaškrtávacích políčok v ľavom stĺpci.&lt;/p&gt;
@@ -197,34 +246,47 @@
 &lt;/html&gt;</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="64"/>
         <source>Choose</source>
         <translation>Vybrať</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="69"/>
         <source>PkgName</source>
         <translation>Názov balíčka</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="74"/>
         <source>Version</source>
         <translation>Verzia</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="79"/>
         <source>Category</source>
         <translation>Kategória</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="116"/>
+        <source>sched-ext scheduler config</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/km-window.ui" line="123"/>
         <source>Configure</source>
         <translation>Konfigurovať</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="130"/>
         <source>Cancel</source>
         <translation>Zrušiť</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="137"/>
         <source>Execute</source>
         <translation>Spustiť</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="228"/>
         <source>No kernels found!
 Please run `pacman -Sy` to update DB!
 This is needed for the app to work properly</source>
@@ -233,12 +295,15 @@ Prosím, spustite `pacman -Sy` na aktualizáciu DB!
 To je potrebné na správne fungovanie aplikácie</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="202"/>
         <source>Failed to clone repository!
 Please check your internet connection and try again</source>
         <translation>Nepodarilo sa klonovať repozitár!
 Skontrolujte svoje internetové pripojenie a skúste to znova</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="277"/>
+        <location filename="../src/km-window.cpp" line="341"/>
         <source>Please wait...
 We are preparing configuration window for you
 cloning PKGBUILDs..</source>
@@ -247,20 +312,24 @@ Pripravujeme konfiguračné okno pre vás
 klonovanie PKGBUILDs..</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="144"/>
         <source>Failed to initialize alpm handle (%1)</source>
         <translation>Nepodarilo sa inicializovať alpm handle (%1)</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="157"/>
         <source>Failed to release alpm handle (%1)</source>
         <translation>Nepodarilo sa uvoľniť alpm handle (%1)</translation>
     </message>
     <message>
+        <location filename="../src/km-window.cpp" line="362"/>
         <source>Please wait...
 Initializing kernels..</source>
         <translation>Prosím, čakajte...
 Inicializácia jadier..</translation>
     </message>
     <message>
+        <location filename="../src/km-window.ui" line="28"/>
         <source>&lt;html&gt;
 &lt;body&gt;
 &lt;p&gt;Here you&apos;ll see information about currently installed and available Linux kernels.&lt;/p&gt;
